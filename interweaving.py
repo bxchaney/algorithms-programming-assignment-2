c17ce30edd6ab460366fdf737960a5fd77d93852
chaneybx <brandon.chaney@stepstonegroup.com>
2023-12-02 10:54:25 -0500

    adapted to repetitions

--- interweaving.py
+++ interweaving.py
@@ -57,27 +57,39 @@ class Solution:
             return False
         
 def is_interwoven(s: str, x: str, y: str) -> bool:
-    
-    dp = [[False for _ in range(len(y)+1)] for _ in range(len(x) + 1)]
-    
-    if len(x) + len(y) != len(s):
+
+    x_k = x * (len(s) // len(x))
+    y_k = y * (len(s) // len(y))
+
+    dp = [[False for _ in range(len(y_k)+1)] for _ in range(len(x_k) + 1)]
+
+    if len(x) + len(y) > len(s):
         return False
-    for i in range(len(x)+1):
-        for j in range(len(y)+1):
+    for i in range(len(x_k)+1):
+        for j in range(len(y_k)+1):
+            if i + j > len(s):
+                continue
             if i == 0 and j == 0:
                 dp[i][j] = True
+            elif i == 0:
+                dp[i][j] = dp[i][j - 1] and y[(j - 1)%len(y)] == s[i + j - 1]
             elif j == 0:
-                dp[i][j] = dp[i - 1][j] and x[i - 1] == s[i + j - 1]
+                dp[i][j] = dp[i - 1][j] and x[(i - 1)%len(x)] == s[i + j - 1]
             else:
-                dp[i][j] = ((dp[i - 1][j] and x[i - 1] == s[i + j - 1]) or
-                               (dp[i][j - 1] and y[j - 1] == s[i + j - 1]))
+                dp[i][j] = (
+                    (dp[i - 1][j] and x[(i - 1)%len(x)] == s[i + j - 1]) or
+                    (dp[i][j - 1] and y[(j - 1)%len(y)] == s[i + j - 1])
+                )
                 
-    print(dp)
+    for row in dp:
+        print(
+            [1 if i else 0 for i in row ]
+            )
     return dp[len(x)][len(y)]
 
 if __name__ == '__main__':
-    str1 = "101"
-    str2 = "010"
-    str3 = "011010"
+    str1 = "11011"
+    str2 = "0"
+    str3 = "00110110010101101100110"
     #print(Solution.isInterleave(str1, str2, str3))
     print(is_interwoven(str3, str1, str2))
